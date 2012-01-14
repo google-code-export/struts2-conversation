b5785d4db5a68469997040f26d71ab9d7ee4b4ce
reesbyars@gmail.com <reesbyars@gmail.com@24914187-08e4-e770-3948-5fb7bb0b037c>
2012-01-14 04:05:48 +0000

    Began adding thorough javadoc.  Good start, lots to go.

--- struts2-conversation-plugin/src/main/java/com/google/code/struts2/scope/sessionfield/SessionFieldUtil.java
+++ struts2-conversation-plugin/src/main/java/com/google/code/struts2/scope/sessionfield/SessionFieldUtil.java
@@ -3,20 +3,48 @@ package com.google.code.struts2.scope.sessionfield;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+import com.google.code.struts2.scope.conversation.ConversationField;
 import com.google.code.struts2.scope.util.ReflectionUtil;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.util.logging.Logger;
 import com.opensymphony.xwork2.util.logging.LoggerFactory;
 
+/**
+ * 
+ * A utility class that provides static methods that are used internally and
+ * for unit testing.  Usage of this utility in a outside of these contexts is
+ * discouraged.  Most of the methods are not optimized for other uses.
+ * 
+ * @author rees.byars
+ *
+ */
 public class SessionFieldUtil {
 	
 	private static final Logger LOG = LoggerFactory
 		.getLogger(SessionFieldUtil.class);
 	
+	/**
+	 * Given the name of a session-scoped field and its class, this method
+	 * returns the appropriate key that is used to identify instances of the 
+	 * field in the session-field map.
+	 * 
+	 * @param name
+	 * @param clazz
+	 * @return
+	 */
 	public static String buildKey(String name, Class<?> clazz) {
 		return clazz.getName() + "." + name;
 	}
 	
+	/**
+	 * Given a session field's name and class, the value of the field is
+	 * returned from the session.
+	 * 
+	 * @param <T>
+	 * @param name
+	 * @param clazz
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T getSessionField(String name, Class<T> clazz) {
 		String key = buildKey(name, clazz);
@@ -27,6 +55,13 @@ public class SessionFieldUtil {
 		return (T) sessionFieldMap.get(key);
 	}
 	
+	/**
+	 * Given a session field's name and an instance, the value is set
+	 * for the field in the session.
+	 * 
+	 * @param fieldName
+	 * @param fieldValue
+	 */
 	public static void setSessionField(String name, Object sessionField) {
 		String key = buildKey(name, sessionField.getClass());
 		@SuppressWarnings("unchecked")
@@ -37,6 +72,12 @@ public class SessionFieldUtil {
 		sessionFieldMap.put(key, sessionField);
 	}
 	
+	/**
+	 * The current values of session fields annotated with {@link SessionField} 
+	 * are extracted from the target object and placed into the session. 
+	 * 
+	 * @param target
+	 */
 	public static void extractSessionFields(Object target) {
 		Class<?> clazz = target.getClass();
 		for (Field field : ReflectionUtil.getFields(clazz)) {
@@ -57,6 +98,12 @@ public class SessionFieldUtil {
 		}
 	}
 	
+	/**
+	 * The target object's session fields that are annotated with 
+	 * {@link SessionField} are injected from the session. 
+	 * 
+	 * @param target
+	 */
 	public static void injectSessionFields(Object target) {
 		for (Field field : ReflectionUtil.getFields(target.getClass())) {
 			if (field.isAnnotationPresent(SessionField.class)) {
@@ -73,6 +120,12 @@ public class SessionFieldUtil {
 		}
 	}
 	
+	/**
+	 * Returns the name of the given field's SessionField
+	 * 
+	 * @param field
+	 * @return
+	 */
 	protected static String getSessionFieldName(Field field) {
 		String name = field.getAnnotation(SessionField.class).name();
 		if (name.equals(SessionField.DEFAULT)) {
