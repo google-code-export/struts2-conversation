ab49f1571983a1ed0c9df373b93d6d860301e525
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2012-02-11 20:47:01 +0000

    Had a bad import

--- trunk/struts2-conversation-plugin/src/test/java/com/google/code/rees/scope/mocks/actions/MockConventionController.java
+++ trunk/struts2-conversation-plugin/src/test/java/com/google/code/rees/scope/mocks/actions/MockConventionController.java
@@ -2,71 +2,69 @@ package com.google.code.rees.scope.mocks.actions;
 
 import org.apache.struts2.convention.annotation.Action;
 
-import com.google.code.rees.scope.request.RequestField;
 import com.google.code.rees.scope.session.SessionField;
 
 public class MockConventionController extends AbstractController {
-	
-	public static final String SUCCESS = "success";
-	public static final String IN_PROGRESS = "in.progress";
-	public static final String NOT_IN_PROGRESS = "not.in.progress";
-	
-	@SessionField
-	private String sessionString = NOT_IN_PROGRESS;
-	
-	@RequestField
-	private int dumb = 2;
-	
-	@SessionField
-	private int smart = 2;
-	
-	@Action(value="begin")
-	public String begin() {
-		setSessionString(IN_PROGRESS);
-		smart = 3;
-		setDumb(3);
-		setWorkflowString("hungee");
-		return SUCCESS;
-	}
-	
-	@Action(value="continue1")
-	public String continue1() {
-		return SUCCESS;
-	}
 
-	@Action(value="continue2")
-	public String continue2() {
-		return SUCCESS;
-	}
-	
-	@Action(value="end")
-	public String end() {
-		setSessionString(NOT_IN_PROGRESS);
-		return SUCCESS;
-	}
+    public static final String SUCCESS = "success";
+    public static final String IN_PROGRESS = "in.progress";
+    public static final String NOT_IN_PROGRESS = "not.in.progress";
 
-	public void setSessionString(String sessionString) {
-		this.sessionString = sessionString;
-	}
+    @SessionField
+    private String sessionString = NOT_IN_PROGRESS;
 
-	public String getSessionString() {
-		return sessionString;
-	}
+    private int dumb = 2;
 
-	public void setDumb(int dumb) {
-		this.dumb = dumb;
-	}
+    @SessionField
+    private int smart = 2;
 
-	public int getDumb() {
-		return dumb;
-	}
-	
-	public void setSmart(int smart) {
-		this.smart = smart;
-	}
-	
-	public int getSmart() {
-		return smart;
-	}
+    @Action(value = "begin")
+    public String begin() {
+        setSessionString(IN_PROGRESS);
+        smart = 3;
+        setDumb(3);
+        setWorkflowString("hungee");
+        return SUCCESS;
+    }
+
+    @Action(value = "continue1")
+    public String continue1() {
+        return SUCCESS;
+    }
+
+    @Action(value = "continue2")
+    public String continue2() {
+        return SUCCESS;
+    }
+
+    @Action(value = "end")
+    public String end() {
+        setSessionString(NOT_IN_PROGRESS);
+        return SUCCESS;
+    }
+
+    public void setSessionString(String sessionString) {
+        this.sessionString = sessionString;
+    }
+
+    public String getSessionString() {
+        return sessionString;
+    }
+
+    public void setDumb(int dumb) {
+        this.dumb = dumb;
+    }
+
+    public int getDumb() {
+        return dumb;
+    }
+
+    public void setSmart(int smart) {
+        this.smart = smart;
+    }
+
+    public int getSmart() {
+        return smart;
+    }
 
 }
