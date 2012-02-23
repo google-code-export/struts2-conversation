a32e24723fce4f9f515955772a7159bd44762654
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2012-02-23 21:31:04 +0000

    Initial import of the junit4 plugin

---
+++ struts2-junit4-plugin/src/main/java/com/google/code/struts2/test/ProxyExecutionListener.java
@@ -0,0 +1,15 @@
+package com.google.code.struts2.test;
+
+import com.opensymphony.xwork2.ActionInvocation;
+
+public interface ProxyExecutionListener {
+	
+	/**
+	 * Similar to a PreResultListener, but guaranteed to be called after all the
+	 * beforeResult methods of the PreResultListeners.
+	 * 
+	 * @param invocation
+	 * @param result
+	 */
+	public void afterProxyExecution(ActionInvocation invocation, String result);
+}
