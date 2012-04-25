301ed3604ef1863babb7f368b35ce6b6c30a3e34
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2012-04-25 23:42:51 +0000

    [maven-release-plugin]  copy for tag struts2-conversation-scope-plugin-1.5.4

--- playground/src/test/java/org/byars/struts2/actions/selenium/SeleniumThreadFactory.java
+++
@@ -1,9 +0,0 @@
-package org.byars.struts2.actions.selenium;
-
-public interface SeleniumThreadFactory {
-
-    public void setSeleniumFactory(SeleniumFactory seleniumFactory);
-
-    public SeleniumThread createThread();
-
-}
