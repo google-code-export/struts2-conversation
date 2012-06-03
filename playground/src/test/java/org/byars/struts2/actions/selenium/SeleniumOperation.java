3cf7f0a9bb74cb0f90c08087ae70e7c41eaf8b01
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2012-06-03 22:09:04 +0000

    [maven-release-plugin]  copy for tag struts2-conversation-config-browser-extension-1.6.0

--- playground/src/test/java/org/byars/struts2/actions/selenium/SeleniumOperation.java
+++
@@ -1,9 +0,0 @@
-package org.byars.struts2.actions.selenium;
-
-import com.thoughtworks.selenium.Selenium;
-
-public interface SeleniumOperation {
-
-    public void doOperation(Selenium selenium);
-
-}
