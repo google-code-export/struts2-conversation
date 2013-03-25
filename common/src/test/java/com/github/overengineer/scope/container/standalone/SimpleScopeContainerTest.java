115c0fd5c626d139220f303c7b1f4c82ca1eee7b
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2013-03-25 13:29:50 +0000

    2.x, added some tests for the container, tweaked the container exception handling for better messages to support easy debugging of wiring issues, removed the common module from automatic loading in the container

--- common/src/test/java/com/github/overengineer/scope/container/standalone/SimpleScopeContainerTest.java
+++ common/src/test/java/com/github/overengineer/scope/container/standalone/SimpleScopeContainerTest.java
@@ -1,6 +1,9 @@
 package com.github.overengineer.scope.container.standalone;
 
+import com.github.overengineer.scope.CommonModule;
 import com.github.overengineer.scope.container.ScopeContainer;
+import com.github.overengineer.scope.monitor.ScheduledExecutorTimeoutMonitor;
+import com.github.overengineer.scope.monitor.TimeoutMonitor;
 import org.junit.Test;
 
 /**
@@ -9,11 +12,24 @@ public class SimpleScopeContainerTest {
 
 
     @Test
-    public void testVerify() throws WiringException {
+    public void testVerify_positive() throws WiringException {
 
         StandaloneContainer container = new SimpleScopeContainer();
 
-        container.addInstance(ScopeContainer.class, container);
+        container.verify();
+
+        container.loadModule(new CommonModule());
+
+        container.verify();
+
+    }
+
+    @Test(expected = WiringException.class)
+    public void testVerify_negative() throws WiringException {
+
+        StandaloneContainer container = new SimpleScopeContainer();
+
+        container.add(TimeoutMonitor.class, ScheduledExecutorTimeoutMonitor.class);
 
         container.verify();
 
--- struts2/src/main/java/com/github/overengineer/scope/struts2/StrutsModuleContainer.java
+++ struts2/src/main/java/com/github/overengineer/scope/struts2/StrutsModuleContainer.java
@@ -1,5 +1,6 @@
 package com.github.overengineer.scope.struts2;
 
+import com.github.overengineer.scope.CommonModule;
 import com.github.overengineer.scope.container.standalone.SimpleScopeContainer;
 import com.github.overengineer.scope.conversation.ConversationModule;
 import com.github.overengineer.scope.conversation.configuration.ConversationArbitrator;
@@ -10,6 +11,7 @@ public class StrutsModuleContainer extends SimpleScopeContainer {
     private static final long serialVersionUID = 3180479652636319036L;
 
     public StrutsModuleContainer() {
+        loadModule(new CommonModule());
         loadModule(new SessionModule());
         loadModule(new ConversationModule());
         add(ConversationArbitrator.class, StrutsConversationArbitrator.class);
