4a5d67f90f083c2729276570c55c2a616d943a63
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2013-03-25 14:03:39 +0000

    2.x, finished first pass of basic tests for the standalone container

--- common/src/test/java/com/github/overengineer/scope/container/standalone/SimpleScopeContainerTest.java
+++ common/src/test/java/com/github/overengineer/scope/container/standalone/SimpleScopeContainerTest.java
@@ -1,15 +1,32 @@
 package com.github.overengineer.scope.container.standalone;
 
 import com.github.overengineer.scope.CommonModule;
-import com.github.overengineer.scope.container.ScopeContainer;
+import com.github.overengineer.scope.monitor.DefaultSchedulerProvider;
 import com.github.overengineer.scope.monitor.ScheduledExecutorTimeoutMonitor;
+import com.github.overengineer.scope.monitor.SchedulerProvider;
 import com.github.overengineer.scope.monitor.TimeoutMonitor;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  */
 public class SimpleScopeContainerTest {
 
+    @Test
+    public void testLoadModule() {
+
+        StandaloneContainer container = new SimpleScopeContainer();
+
+        container.loadModule(new CommonModule());
+
+        TimeoutMonitor monitor = container.getComponent(TimeoutMonitor.class);
+
+        assertNotNull(monitor);
+
+    }
 
     @Test
     public void testVerify_positive() throws WiringException {
@@ -32,6 +49,45 @@ public class SimpleScopeContainerTest {
         container.add(TimeoutMonitor.class, ScheduledExecutorTimeoutMonitor.class);
 
         container.verify();
+
+    }
+
+    @Test
+    public void testAddAndGetComponent() {
+
+        StandaloneContainer container = new SimpleScopeContainer();
+
+        container.add(SchedulerProvider.class, DefaultSchedulerProvider.class);
+
+        SchedulerProvider provider = container.getComponent(SchedulerProvider.class);
+
+        assertTrue(provider instanceof DefaultSchedulerProvider);
+
+    }
+
+    @Test
+    public void testAddAndGetInstance() {
+
+        StandaloneContainer container = new SimpleScopeContainer();
+
+        SchedulerProvider given = new DefaultSchedulerProvider();
+
+        container.addInstance(SchedulerProvider.class, given);
+
+        SchedulerProvider received = container.getComponent(SchedulerProvider.class);
+
+        assertEquals(given, received);
+
+    }
+
+    @Test
+    public void testAddAndGetProperty() {
+
+        StandaloneContainer container = new SimpleScopeContainer();
+
+        container.addProperty("test", 69L);
+
+        assertEquals((Long) 69L, container.getProperty(Long.TYPE, "test"));
 
     }
 
