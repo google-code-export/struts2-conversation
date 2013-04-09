83e4452d64832031828cd573c364da873f24fe6b
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2013-04-09 15:00:50 +0000

    2.x, for the container, added spring comparison test

--- common/src/main/java/com/github/overengineer/scope/container/proxy/ProxyHandlerFactory.java
+++ common/src/main/java/com/github/overengineer/scope/container/proxy/ProxyHandlerFactory.java
@@ -1,8 +1,10 @@
 package com.github.overengineer.scope.container.proxy;
 
+import java.io.Serializable;
+
 /**
  */
-public interface ProxyHandlerFactory {
+public interface ProxyHandlerFactory extends Serializable {
 
     <T> ComponentProxyHandler<T> createProxy(Class<?> targetClass);
 
