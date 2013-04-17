6bc19559db102a2f34cb922587db44bcfaf17f3a
reesbyars <reesbyars@24914187-08e4-e770-3948-5fb7bb0b037c>
2013-04-17 12:12:42 +0000

    2.x, container, made proxy util real component mehtod recursive in case of embedded proxies

--- common/src/main/java/com/github/overengineer/scope/container/proxy/ProxyUtil.java
+++ common/src/main/java/com/github/overengineer/scope/container/proxy/ProxyUtil.java
@@ -9,7 +9,7 @@ public class ProxyUtil {
     @SuppressWarnings("unchecked")
     public static <T> T getRealComponent(T proxy) {
         if (Proxy.isProxyClass(proxy.getClass())) {
-            return ((ComponentProxyHandler<T>) Proxy.getInvocationHandler(proxy)).getComponent();
+            return getRealComponent(((ComponentProxyHandler<T>) Proxy.getInvocationHandler(proxy)).getComponent());
         }
         return proxy;
     }
